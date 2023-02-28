3f79695289f87a913dd6454b04d203f7650962b6
Robin van der Noord <robinvandernoord@gmail.com>
2023-02-28 17:33:50 +0100

    initial version of this small tool

---
+++ README.md
@@ -0,0 +1,24 @@
+# Opengraph Images
+
+![](example.png)
+
+Requires `wkhtmltopdf` to be installed (`sudo apt install -y wkhtmltopdf`).
+
+By default, the template is pretty simple as can be seen above. 
+`template.html` can be modified or a different option can be passed to `generate_image`.
+This template is rendered using [yatl](https://github.com/web2py/yatl).  
+`wkhtmltopdf` supports most styles a browser would!
+
+## Usage
+
+```python
+from opengraph_images import generate_image
+
+img: bytes = generate_image({
+    "title": "Hello World",
+    "icon": "https://example.com/icon.png",
+    "img": "https://example.com/image.png",
+    # optional:
+    "template": "other_template.html",
+})
+```
---
+++ create.py
@@ -0,0 +1,53 @@
+#
+import typing
+from typing_extensions import NotRequired
+
+import imgkit
+import yatl
+
+
+class Context(typing.TypedDict):
+    icon: str
+    title: str
+    img: str
+
+    template: NotRequired[str]
+
+
+def generate_image(
+    ctx: Context
+):
+    options = {
+        'format': 'png',
+        # opengraph image size is 1200x630:
+        'width': '1200',
+        'crop-w': '1200',
+        'crop-h': '630',
+        # 'crop-x': '20',
+        # 'crop-y': '20',
+        'encoding': "UTF-8",
+    }
+
+    with open(ctx.get("template", "template.html")) as f:
+        rendered = yatl.render(stream=f,
+                               context=ctx,
+                               delimiters="[[ ]]")
+
+    return imgkit.from_string(rendered, False, options=options)
+
+
+def demo():
+    img = generate_image(
+        dict(
+            icon="https://news.su6.nl/static/icons/nu.svg",
+            title="Door deze ontdekking gaat je telefoon straks tot twee keer zo lang mee",
+            img="https://media.nu.nl/m/zh2x98aada6s_sqr256.jpg/door-deze-ontdekking-gaat-je-telefoon-straks-tot-twee-keer-zo-lang-mee.jpg",
+        )
+    )
+
+    with open("temp.png", "wb") as f:
+        f.write(img)
+
+
+if __name__ == '__main__':
+    demo()
